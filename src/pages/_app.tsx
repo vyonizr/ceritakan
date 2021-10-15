300b1e115072457a99f2cc96aec863d620b1d0e1
Fitrahtur Rahman <fitrahtur@gmail.com>
2021-10-16 04:51:07 +0700

    change GTM method

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,15 +1,48 @@
 import { useEffect } from 'react'
+import Script from 'next/script'
+import { useRouter } from 'next/router'
+import * as gtag from 'src/config/gtag'
 import 'normalize.css'
 import 'src/styles/tailwind.css'
 import 'src/styles/globals.css'
 import type { AppProps } from 'next/app'
-import TagManager from 'react-gtm-module'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  useEffect(() => {
-    TagManager.initialize({ gtmId: 'G-Z4ZDV08XWZ' })
-  }, [])
+  const router = useRouter()
 
-  return <Component {...pageProps} />
+  useEffect(() => {
+    const handleRouteChange = (url: string) => {
+      gtag.pageview(url)
+    }
+    router.events.on('routeChangeComplete', handleRouteChange)
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange)
+    }
+  }, [router.events])
+
+  return (
+    <>
+      {/* Global Site Tag (gtag.js) - Google Analytics */}
+      <Script
+        strategy='afterInteractive'
+        src={`https://www.googletagmanager.com/gtag/js?id=${gtag.GA_TRACKING_ID}`}
+      />
+      <Script
+        id='gtag-init'
+        strategy='afterInteractive'
+        dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${gtag.GA_TRACKING_ID}', {
+              page_path: window.location.pathname,
+            });
+          `,
+        }}
+      />
+      <Component {...pageProps} />
+    </>
+  )
 }
 export default MyApp
