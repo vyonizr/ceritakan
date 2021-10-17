6e11dd9bc0d0d9e3746763c75ca7c51cc1877e19
Fitrahtur Rahman <fitrahtur@gmail.com>
2021-10-17 16:21:56 +0700

    break down components

--- src/helpers/getRandomFloat.ts
+++ src/helpers/getRandomFloat.ts
@@ -1,4 +1,12 @@
-function getRandomFloat(min: number, max: number) {
+import {
+  CARD_MAX_ROTATE_DEGREE,
+  CARD_MIN_ROTATE_DEGREE,
+} from 'src/common/constants'
+
+function getRandomFloat(
+  min = CARD_MIN_ROTATE_DEGREE,
+  max = CARD_MAX_ROTATE_DEGREE
+) {
   const randomFloat = Math.random() * (max - min) + min
 
   return randomFloat.toFixed(1)
--- src/helpers/getRandomIntInclusive.ts
+++ src/helpers/getRandomIntInclusive.ts
@@ -1,4 +1,12 @@
-function getRandomIntInclusive(min: number, max: number) {
+import {
+  CARD_MAX_ROTATE_DEGREE,
+  CARD_MIN_ROTATE_DEGREE,
+} from 'src/common/constants'
+
+function getRandomIntInclusive(
+  min = CARD_MIN_ROTATE_DEGREE,
+  max = CARD_MAX_ROTATE_DEGREE
+) {
   min = Math.ceil(min)
   max = Math.floor(max)
   return Math.floor(Math.random() * (max - min + 1) + min)
