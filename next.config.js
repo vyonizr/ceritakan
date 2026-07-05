95ee2ffe1fb94241ad70861c85b9f15d1ff50ad8
Fitrahtur Rahman <fitrahtur@gmail.com>
2026-07-05 21:58:30 +0700

    drop PWA

--- next.config.js
+++ next.config.js
@@ -1,16 +1,5 @@
 /** @type {import('next').NextConfig} */
-const isDevelopment = process.env.NODE_ENV === 'development'
-
-const baseConfig = {
+module.exports = {
   reactStrictMode: true,
   outputFileTracingRoot: __dirname,
-}
-
-if (isDevelopment) {
-  module.exports = baseConfig
-} else {
-  const withPWA = require('@ducanh2912/next-pwa').default({
-    dest: 'public',
-  })
-  module.exports = withPWA(baseConfig)
 }
